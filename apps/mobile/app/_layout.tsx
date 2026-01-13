313eb3e95452ef203cae3cfe77d00477bdc1c185
Anwar3006 <anwarsadat.d2@gmail.com>
2026-01-13 21:16:40 +0000

    feat: Added SplashScreen to rootlayout

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,5 +1,15 @@
 import "../global.css";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 200,
+  fade: true,
+});
 
 export default function RootLayout() {
   return <Stack />;
